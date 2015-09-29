6c03849d7a8b3b1ea621150beaf2579b3df60a2e
Ice Wolf <github@deep-freeze.ca>
2015-09-28 17:21:04 -0700

    Add Default Mode to FileStream constructor.

--- src/FileStream.php
+++ src/FileStream.php
@@ -48,7 +48,7 @@ class FileStream extends StreamDecorator implements StreamInterface
   );
 
 
-  public function __construct($path, $fileMode, $fileAccess) {
+  public function __construct($path, $fileMode, $fileAccess=self::MODE_OPEN) {
     $this->init($path, $fileMode, $fileAccess);
   }
 
